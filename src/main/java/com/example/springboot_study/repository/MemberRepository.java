a5fb9f10d44304ae5a1030cfed0ccde87202dc7a
lim-youn-ho <limyounhoo@gmail.com>
2024-06-25 17:16:06 +0900

    스프링부트 스터디-기본구조 추가

---
+++ src/main/java/com/example/springboot_study/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot_study.repository;
+
+import com.example.springboot_study.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
