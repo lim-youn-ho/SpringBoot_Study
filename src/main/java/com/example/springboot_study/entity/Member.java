9e3bf9d32be650f45b7d7215e1eb618be97d45eb
lim-youn-ho <limyounhoo@gmail.com>
2024-07-11 16:29:32 +0900

    스프링부트 스터디-JPA 기본crud

--- src/main/java/com/example/springboot_study/entity/Member.java
+++ src/main/java/com/example/springboot_study/entity/Member.java
@@ -1,20 +1,25 @@
 package com.example.springboot_study.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Entity
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키 자동으로1씩 증가
     @Column(name = "id" , updatable = false)
     private Long id;
 
     @Column(name = "name" , nullable = false)
     private  String name;
+
+    public void changeName(String name){
+        this.name = name;
+    }
 }
--- src/test/java/com/example/springboot_study/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/springboot_study/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.springboot_study.repository;
 
 import com.example.springboot_study.entity.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -18,6 +19,7 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    //조회
     @Sql("/insert-members.sql")
     @Test
     void getMemberByName(){
@@ -28,6 +30,7 @@ class MemberRepositoryTest {
 
     }
 
+    //저장
     @Test
     void saveMember(){
 
@@ -38,5 +41,33 @@ class MemberRepositoryTest {
         assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
 
     }
+
+    //삭제
+    @Sql("/insert-members.sql")
+    @Test
+    @AfterEach//deleteAll 사용시 AfterEach어노테이션 사용
+    void deleteMemberById(){
+        //단건
+        memberRepository.deleteById(2L);
+        assertThat(memberRepository.findById(2L).isEmpty()).isTrue();
+
+        //전체(테스트 코드에서만 실행)
+        memberRepository.deleteAll();
+        assertThat(memberRepository.findAll().size()).isZero();
+
+    }
+
+    //업데이트
+    @Sql("/insert-members.sql")
+    void update(){
+        Member member = memberRepository.findById(2L).get();
+
+        member.changeName("BC");
+
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
+
+    }
+
+
 
 }
