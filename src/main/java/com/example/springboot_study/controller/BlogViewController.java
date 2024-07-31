dcbf60fb89fd9115dfb1453259d2ffad6000657e
lim-youn-ho <limyounhoo@gmail.com>
2024-07-31 12:58:50 +0900

    스프링부트 스터디-블로그 글 생성 시각 저장

--- src/main/java/com/example/springboot_study/controller/BlogViewController.java
+++ src/main/java/com/example/springboot_study/controller/BlogViewController.java
@@ -1,11 +1,13 @@
 package com.example.springboot_study.controller;
 
+import com.example.springboot_study.domain.Article;
 import com.example.springboot_study.dto.ArticleListViewResponse;
 import com.example.springboot_study.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,6 +27,14 @@ public class BlogViewController {
 
         return "articleList";// resource/templates/articleList.html 찾게 리턴
 
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id , Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleListViewResponse(article));
+
+        return "article";
     }
 
 }
--- src/main/java/com/example/springboot_study/dto/ArticleListViewResponse.java
+++ src/main/java/com/example/springboot_study/dto/ArticleListViewResponse.java
@@ -3,17 +3,21 @@ package com.example.springboot_study.dto;
 import com.example.springboot_study.domain.Article;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ArticleListViewResponse {
 
     private final Long id;
     private final String title;
     private final String content;
+    private LocalDateTime createdAt;
 
     public ArticleListViewResponse(Article article){
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
     }
 
 }
