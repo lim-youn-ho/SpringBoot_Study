d4e37665cba390bcf6c0685c8c9386d3ba4cd322
lim-youn-ho <limyounhoo@gmail.com>
2024-08-29 14:15:41 +0900

    스프링부트 스터디-lambda01-2

--- src/main/java/com/example/springboot_study/lambda/lambda01.java
+++ src/main/java/com/example/springboot_study/lambda/lambda01.java
@@ -2,6 +2,11 @@ package com.example.springboot_study.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.BaseStream;
+import java.util.stream.Collector;
 
 public class lambda01 {
 
@@ -19,6 +24,28 @@ public class lambda01 {
         }
         //람다식으로 간결하게
         list.forEach(num -> System.out.println(num));
+
+        //람다식_필터 짝수출력시
+        list.stream()
+                .filter(num -> num % 2 == 0)
+                .forEach(evenNum -> System.out.println(evenNum));
+    }
+
+
+    //자바에서 기본적으로제공하는 함수형 인터페이스를 받는다
+    public interface Stream<T> extends BaseStream<T, Stream<T>> {
+
+        Stream<T> filter(Predicate<? super T> predicate);
+
+        <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+
+        void forEach(Consumer<? super T> action);
+
+        <R, A> R collect(Collector<? super T, A, R> collector);
+
+
     }
 
 }
+
+
