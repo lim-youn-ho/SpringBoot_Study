8177f87a81b18369d8ee6a8bd637b3e46184fb57
lim-youn-ho <limyounhoo@gmail.com>
2024-09-10 15:58:19 +0900

    업무용 프로그램

--- src/main/java/com/example/springboot_study/controller/HanbillApiController.java
+++ src/main/java/com/example/springboot_study/controller/HanbillApiController.java
@@ -1,8 +1,27 @@
 package com.example.springboot_study.controller;
 
+import com.example.springboot_study.dto.ArticleResponse;
+import com.example.springboot_study.service.HanbillApiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class HanbillApiController {
+    HanbillApiService hanbillApiService;
+    @GetMapping("/api/getArticles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+
+        List<ArticleResponse> articles = hanbillApiService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
 
 
+        return ResponseEntity.ok()
+                .body(articles);
+
+    }
 
 
 }
--- src/main/java/com/example/springboot_study/service/HanbillApiService.java
+++ src/main/java/com/example/springboot_study/service/HanbillApiService.java
@@ -1,4 +1,18 @@
 package com.example.springboot_study.service;
 
+import com.example.springboot_study.domain.Article;
+import com.example.springboot_study.repository.BlogRepository;
+
+import java.util.List;
+
 public class HanbillApiService {
+    private  final BlogRepository blogRepository;
+
+    public HanbillApiService(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
 }
